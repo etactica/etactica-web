881de480dc895c507d11a6011a2c442fe7672f89
Sigríður Hrafnsdóttir <sigridur@remake.local>
2016-01-19 17:08:16 +0000

    update er cool

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,5 +1,7 @@
 import {Component} from 'angular2/core';
 import {Hero} from './hero';
+import {HeroService} from './hero.service';
+
 
 @Component({
     selector: 'my-hero-detail',
@@ -11,10 +13,19 @@ import {Hero} from './hero';
         <label>name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
       </div>
+      <div><label>details: </label>{{hero.details}}</div>
+      <div><label>ranking: </label>{{hero.ranking}}</div>
+      <button (click)="postHero(hero)">Save</button>
     </div>
   `,
     inputs: ['hero']
 })
 export class HeroDetailComponent {
     public hero: Hero;
+
+  constructor(private _heroService: HeroService) {
+  }
+    postHero(hero) {
+    this._heroService.postHeroes(hero);
+  }
 }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,7 +1,7 @@
 import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 import {Injectable} from 'angular2/core';
-import {Http} from 'angular2/http';
+import {Http, Headers} from 'angular2/http'; 
 import 'rxjs/Rx';
 
 @Injectable()
@@ -10,6 +10,38 @@ export class HeroService {
     constructor(public http: Http) {}
 
     getHeroes() {
-        return this.http.get('http://localhost:4000/list_of_heroes/').map((data) => data.json())
+        return this.http.get('http://localhost:8888/list_of_heroes/').map((data) => data.json())
     }
+
+    putHeroes(hero: Hero){
+
+		var headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+
+    	this.http.put('http://localhost:8888/list_of_heroes/' + hero.id, 
+		JSON.stringify(hero),
+		{headers:headers})
+		.map(res => res.json())
+		.subscribe(res => {this.hero = res.json});
+		}
+
+    postHeroes(hero: Hero){
+    	console.log(hero);
+    	console.log(JSON.stringify(hero));
+
+		var headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+
+    	this.http.post('http://localhost:8888/list_of_heroes/', 
+			JSON.stringify(hero),
+			{headers:headers})
+			.map(res => res.json())
+			.subscribe(res => {this.hero = res.json});
+	}
+
+	/*
+	updateHero(id: number, hero: Hero) {
+		return this.http.put('http://localhost:8888/list_of_heroes/' + id, 'PUT', hero);
+	}*/
 }
+
