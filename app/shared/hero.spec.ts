72c29d9b400c64418b36a811e1f5b43b06ade939
siggahrafns <sh@etactica.com>
2016-01-25 14:42:05 +0000

    Added unit test for Hero, using Jasmine

---
+++ app/shared/hero.spec.ts
@@ -0,0 +1,19 @@
+import {Hero} from './hero';
+    describe('Hero', () => {
+        it('has the id given in the constructor', function() {
+            var hero = new Hero(1, 'Super Cat', 'master', 20);
+            expect(hero.id).toEqual(1);
+        });
+        it('has name given in the constructor', function() {
+            var hero = new Hero(1, 'Super Cat', 'master', 20);
+            expect(hero.name).toEqual('Super Cat');
+        });
+        it('has details given in the constructor', function() {
+            var hero = new Hero(1, 'Super Cat', 'master', 20);
+            expect(hero.details).toEqual('master');
+        });
+        it('has ranking given in the constructor', function() {
+            var hero = new Hero(1, 'Super Cat', 'master', 20);
+            expect(hero.ranking).toEqual(20);
+        });
+})
