a2e780226374042e9fea3d62dd2c8eaa8084046b
magnus <magnus@remake.is>
2016-01-26 13:27:12 +0000

    api calls to aws

--- app/shared/hero.service.ts
+++ app/shared/hero.service.ts
@@ -5,24 +5,35 @@ import 'rxjs/Rx';
 
 @Injectable()
 export class HeroService {
+    private aws:string = 'https://bx2zwhpoi0.execute-api.eu-west-1.amazonaws.com/prod/heroes/';
+    private local:string = 'http://localhost:8888/list_of_heroes/';
+    private useLocal: boolean = false;
 
     constructor(public http:Http) {
     }
 
+    path(){
+        return this.useLocal ? this.local : this.aws;
+    }
+
     getHeroes() {
-        return this.http.get('http://localhost:8888/list_of_heroes/').map((data) => data.json())
+        return this.http.get(this.path()).map((data) => data.json())
+    }
+
+    getHero(id:number){
+        return this.http.get(this.path() + id).map((data) => data.json())
     }
 
     putHero(hero:Hero) {
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
-
-        return this.http.put('http://localhost:8888/list_of_heroes/' + hero.id,
+        return this.http.put(this.path() + hero.id,
             JSON.stringify(hero),
             {headers: headers});
     }
 
     postHero(hero:Hero) {
+        console.log(JSON.stringify(hero));
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
         return this.http.post('http://localhost:8888/list_of_heroes',
@@ -31,6 +42,6 @@ export class HeroService {
     }
 
     deleteHero(hero:Hero) {
-        return this.http.delete('http://localhost:8888/list_of_heroes/' + hero.id);
+        return this.http.delete(this.path() + hero.id);
     }
 }
